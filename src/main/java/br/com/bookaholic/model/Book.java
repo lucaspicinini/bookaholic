99ef3367cbe41c116c666e65602562cb6ad3f54c
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-05-29 11:40:27 -0300

    implements Author in Book class for future relationship in database

--- src/main/java/br/com/bookaholic/model/Book.java
+++ src/main/java/br/com/bookaholic/model/Book.java
@@ -1,17 +1,53 @@
 package br.com.bookaholic.model;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Book {
-    int id;
-    String title;
-    // List<Author> authors;
-    List<String> subjects = new ArrayList<>();
-    boolean copyright;
-    int downloadCount;
+    private Integer id;
+    private String title;
+    private Boolean copyright;
+    private Integer downloadCount;
+    private List<String> subjects;
+    private List<Author> authors;
 
-    public Book(Object book) {
+    public Book(BookData book) {
+        this.id = book.id();
+        this.title = book.title();
+        this.copyright = book.copyright();
+        this.downloadCount = book.downloadCount();
+        this.subjects = book.subjects();
+        this.authors = book.authors().stream().map(Author::new).toList();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCopyright() {
+        return copyright;
+    }
+
+    public int getDownloadCount() {
+        return downloadCount;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
     }
 }
