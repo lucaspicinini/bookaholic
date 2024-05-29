5d4eedcc47ea673a799e21b26039637479793400
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-05-29 11:39:16 -0300

    change types on deserializing AuthorData

--- src/main/java/br/com/bookaholic/model/AuthorData.java
+++ src/main/java/br/com/bookaholic/model/AuthorData.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record AuthorData(
         String name,
-        @JsonAlias("birth_year") Integer birthYear,
-        @JsonAlias("death_year") Integer deathYear
+        @JsonAlias("birth_year") String birthYear,
+        @JsonAlias("death_year") String deathYear
 ) {
 }
