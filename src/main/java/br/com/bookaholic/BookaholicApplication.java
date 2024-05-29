fbd00e483c883ceb4d6efb2ac0da27b81d0733be
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-05-28 22:19:07 -0300

    Initial commit

---
+++ src/main/java/br/com/bookaholic/BookaholicApplication.java
@@ -0,0 +1,23 @@
+package br.com.bookaholic;
+
+import br.com.bookaholic.entry.EntryPoint;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookaholicApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookaholicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			EntryPoint.exec();
+		} catch (Exception e) {
+            throw new Exception(e);
+        }
+	}
+}
---
+++ src/main/java/br/com/bookaholic/entry/EntryPoint.java
@@ -0,0 +1,7 @@
+package br.com.bookaholic.entry;
+
+public class EntryPoint {
+    public static void exec() {
+        System.out.println("Hello World!");
+    }
+}
