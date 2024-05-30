fda262f42f41d0b03a6259770a7719d051991dce
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-05-30 19:58:50 -0300

    adds method clear terminal screen

---
+++ src/main/java/br/com/bookaholic/utils/ScreenClear.java
@@ -0,0 +1,17 @@
+package br.com.bookaholic.utils;
+
+import java.io.IOException;
+
+public class ScreenClear {
+    public static void clear() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException ex) {
+            ex.getMessage();
+        }
+    }
+}
