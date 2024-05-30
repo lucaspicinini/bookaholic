1f6ee59e31817a4a6024779185ba7195221aa3b6
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-05-30 03:29:11 -0300

    makes EntryPoint shorter for the next sprint

--- src/main/java/br/com/bookaholic/entry/EntryPoint.java
+++ src/main/java/br/com/bookaholic/entry/EntryPoint.java
@@ -2,70 +2,93 @@ package br.com.bookaholic.entry;
 
 import br.com.bookaholic.model.Book;
 import br.com.bookaholic.model.DataIndex;
+import br.com.bookaholic.utils.Catalogue;
+import br.com.bookaholic.utils.CatalogueOptions;
+import br.com.bookaholic.utils.Menu;
 import br.com.bookaholic.service.ApiService;
 import br.com.bookaholic.service.Mapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class EntryPoint {
-    private Scanner scanner = new Scanner(System.in);
-    private ApiService apiService = new ApiService();
-    private Mapper mapper = new Mapper();
-    private String responseBody;
-    private int apiPage = 1;
-    private String apiUrl = "https://gutendex.com/books/?languages=";
-    private String apiLang = "pt";
-    private String apiPagination = "&page=" + apiPage;
+    private final Scanner scanner = new Scanner(System.in);
+    private final ApiService apiService = new ApiService();
+    private final Mapper mapper = new Mapper();
+    private static DataIndex dataIndex;
+    private static Integer apiPageNumber = 1;
+    private static String apiPage = String.valueOf(apiPageNumber);
+    private static String userInput = "";
     private String apiSearchName = "";
-    private String apiSearch = "&search=";
-    private DataIndex dataIndex;
+    private String responseBody;
 
     public void init() {
         boolean exitLoop = false;
 
         while (!exitLoop) {
-            System.out.println("***** Bookaholic *****");
-            System.out.println("Escolha uma opção: ");
-            System.out.println("1 - Exibir catálogo");
-            System.out.println("2 - Buscar por título ou autor");
-            System.out.println();
-            System.out.println("0 - Sair");
-            String userInput = scanner.nextLine();
+            if (userInput.isEmpty()) {
+                Menu.printMainMenu();
+                userInput = scanner.nextLine();
+            }
 
             switch (userInput) {
-                case "0":
-                    System.out.println("Até a próxima :) ...");
-                    exitLoop = true;
                 case "1":
-                    responseBody = apiService.getResponseBody(apiUrl + apiLang + apiPagination);
+                    System.out.println("Estabelecendo conexão...\n");
+                    responseBody = apiService
+                            .getResponseBody("https://gutendex.com/books/?languages=pt&page=" + apiPage);
 
                     if (responseBody != null) {
                         dataIndex = mapper.getClassFromJson(responseBody, DataIndex.class);
-                        List<Book> books = dataIndex.books().stream().map(Book::new).toList();
-                        books.forEach(book -> System.out.println(book.getTitle()));
-
-
-
-//                        books.forEach(book -> book.getAuthors().forEach(author -> System.out.println(author.getName())));
-//                        System.out.println("***** Catálogo Bookaholic *****");
-//                        System.out.println(dataIndex.books());
+                        Catalogue catalogue = new Catalogue(dataIndex);
+                        catalogue.load();
                     }
                     break;
                 case "2":
                     System.out.println("Digite um nome para busca: ");
                     apiSearchName = scanner.nextLine().replace(" ", "%20").toLowerCase();
-                    responseBody = apiService.getResponseBody(apiUrl + apiLang + apiSearch + apiSearchName);
+                    responseBody = apiService
+                            .getResponseBody("https://gutendex.com/books/?languages=pt&search=" + apiSearchName);
                     dataIndex = mapper.getClassFromJson(responseBody, DataIndex.class);
+
                     if (!dataIndex.books().isEmpty()) {
-                        System.out.println(dataIndex.books());
+                        List<Book> books = dataIndex.books().stream().map(Book::new).toList();
+                        books.forEach(Book::printBook);
                     } else {
                         System.out.println("Livro/Autor não encontrado.");
                     }
                     break;
+                case "0":
+                    System.out.println("Até a próxima :) ...\n");
+                    exitLoop = true;
                 default:
                     System.out.println("Digite uma opção válida.");
+                    userInput = "";
             }
         }
+    }
+
+    public static String getApiPage() {
+        return apiPage;
+    }
+
+    public static void setApiPage(String apiPage) {
+        EntryPoint.apiPage = apiPage;
+    }
+
+    public static Integer getApiPageNumber() {
+        return apiPageNumber;
+    }
+
+    public static void setApiPageNumber(Integer apiPageNumber) {
+        EntryPoint.apiPageNumber = apiPageNumber;
+    }
+
+    public static DataIndex getDataIndex() {
+        return dataIndex;
+    }
+
+    public static void setUserInput(String userInput) {
+        EntryPoint.userInput = userInput;
     }
 }
