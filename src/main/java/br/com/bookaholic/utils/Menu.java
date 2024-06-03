5cf45c1d0472a549f14c298ecf57dfcf9bac0d60
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-03 15:50:38 -0300

    add Archive Options to Menu Class

--- src/main/java/br/com/bookaholic/utils/Menu.java
+++ src/main/java/br/com/bookaholic/utils/Menu.java
@@ -1,6 +1,8 @@
 package br.com.bookaholic.utils;
 
 import br.com.bookaholic.entry.EntryPoint;
+import br.com.bookaholic.model.Book;
+import org.springframework.data.domain.Page;
 
 public class Menu {
     private static final String mainMenu = """
@@ -23,15 +25,35 @@ public class Menu {
             5 - Salvar livro por id
             0 - Voltar ao menu principal
             """;
+    private static final String archiveMenu = """
+            +++ Escolha uma opção:
+            1 - Página anterior
+            2 - Pŕoxima página
+            3 - Buscar pelo número da página
+            0 - Voltar ao menu principal
+            """;
 
     // EntryPoint
     public static void mainMenu() {
         System.out.println(mainMenu);
     }
     // EntryPoint
+    public static void archiveMenu() { System.out.println(archiveMenu); }
+    // EntryPoint
     public static void askName() { System.out.println("Digite um nome para busca: "); }
     // EntryPoint
+    public static void askOption() { System.out.println("Selecione uma opção: "); }
+    // EntryPoint
     public static void exit() { System.out.println("Até a próxima :) ...\n"); }
+    // EntryPoint
+    public static void archiveMenuInfo (int pageNumber, Page<Book> page) {
+        System.out.println("\nExibindo página " + pageNumber + " de " + page.getTotalPages());
+        System.out.println("Livros nesta página: " + page.getNumberOfElements());
+        System.out.println("Total de Livros Armazenados: " + page.getTotalElements() + "\n");
+    }
+    public static void invalidPage (Page<Book> page) {
+        System.out.println("Digite uma página entre 1 e " + page.getTotalPages() + ".");
+    }
 
     // Catalogue
     public static void pageNotFound() { System.out.println("Página: " + EntryPoint.getApiPage() + " não encontrada"); }
@@ -40,8 +62,6 @@ public class Menu {
     // Catalogue
     public static void backToCatalogue() { System.out.println("Voltando ao catálogo...\n"); }
 
-    // CatalogueOptions
-    public static void askPage() { System.out.println("Digite o número da página para busca: "); }
     // CatalogueOptions
     public static void askId() { System.out.println("Digite o id do livro: "); }
     // CatalogueOptions
@@ -61,5 +81,7 @@ public class Menu {
     public static void invalidOption() { System.out.println("Digite uma opção válida."); }
     // EntryPoint and CatalogueOptions
     public static void notFound() { System.out.println("Livro/Autor não encontrado."); }
+    // EntryPoint and CatalogueOptions
+    public static void askPage() { System.out.println("Digite o número da página para busca: "); }
 
 }
